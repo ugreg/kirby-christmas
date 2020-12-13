8daed20bdf2888b41a26750fe1878eca0fbcfa3c
Greg <gregory.degruy@gmail.com>
2020-12-13 08:58:57 -0800

    kirby transparent  pallet, xmas tree, star block, clouds

---
+++ colors.lua
@@ -0,0 +1,22 @@
+pico-8 cartridge // http://www.pico-8.com
+version 29
+__lua__
+
+universe.colors = {}
+
+universe.colors.black       = 0
+universe.colors.dark_blue   = 1
+universe.colors.dark_purple = 2
+universe.colors.dark_green  = 3
+universe.colors.brown       = 4
+universe.colors.dark_grey   = 5
+universe.colors.light_grey  = 6
+universe.colors.white       = 7
+universe.colors.red         = 8
+universe.colors.oragne      = 9
+universe.colors.yellow      = 10
+universe.colors.green       = 11
+universe.colors.blue        = 12
+universe.colors.lavender    = 13
+universe.colors.pink        = 14
+universe.colors.light_peach = 15
