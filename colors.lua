faddf824e332b29c50691d144655123cea2ff8f2
Greg <gregory.degruy@gmail.com>
2020-12-18 13:21:25 -0800

    ~ namespace - unused sfx + sfx constants

--- colors.lua
+++ colors.lua
@@ -2,27 +2,27 @@ pico-8 cartridge // http://www.pico-8.com
 version 29
 __lua__
 
-universe.colors = {}
+greguniverse.colors = {}
 
-universe.colors.black       = 0
-universe.colors.dark_blue   = 1
-universe.colors.dark_purple = 2
-universe.colors.dark_green  = 3
-universe.colors.brown       = 4
-universe.colors.dark_grey   = 5
-universe.colors.light_grey  = 6
-universe.colors.white       = 7
-universe.colors.red         = 8
-universe.colors.orange      = 9
-universe.colors.yellow      = 10
-universe.colors.green       = 11
-universe.colors.blue        = 12
-universe.colors.lavender    = 13
-universe.colors.pink        = 14
-universe.colors.light_peach = 15
+greguniverse.colors.black       = 0
+greguniverse.colors.dark_blue   = 1
+greguniverse.colors.dark_purple = 2
+greguniverse.colors.dark_green  = 3
+greguniverse.colors.brown       = 4
+greguniverse.colors.dark_grey   = 5
+greguniverse.colors.light_grey  = 6
+greguniverse.colors.white       = 7
+greguniverse.colors.red         = 8
+greguniverse.colors.orange      = 9
+greguniverse.colors.yellow      = 10
+greguniverse.colors.green       = 11
+greguniverse.colors.blue        = 12
+greguniverse.colors.lavender    = 13
+greguniverse.colors.pink        = 14
+greguniverse.colors.light_peach = 15
 
 -- local transparent = true
 -- if a.k == 72 then
---     palt(universe.colors.blue, transparent)
---     palt(universe.colors.black, not transparent)
+--     palt(greg.universe.colors.blue, transparent)
+--     palt(greg.universe.colors.black, not transparent)
 -- end
---
+++ sfx.lua
@@ -0,0 +1,20 @@
+pico-8 cartridge // http://www.pico-8.com
+version 29
+__lua__
+
+greguniverse.sfx = {}
+
+greguniverse.sfx.fade_out     = 0
+greguniverse.sfx.game_over    = 5
+greguniverse.sfx.jump         = 8
+greguniverse.sfx.get_pickup   = 9
+greguniverse.sfx.break_block  = 10
+greguniverse.sfx.bounce       = 12
+greguniverse.sfx.get_lollipop = 13
+greguniverse.sfx.enemy_death  = 14
+greguniverse.sfx.player_death = 17
+greguniverse.sfx.inhale       = 18
+greguniverse.sfx.actor_hit    = 21
+greguniverse.sfx.level_clear  = 24
+greguniverse.sfx.gem_total    = 25
+greguniverse.sfx.exhale       = 26
